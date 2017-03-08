148cca5ab069e2fae99df83385f82dafb0658332
Greg Izatt <gizatt@mit.edu>
2017-03-07 19:22:39 -0500

    cleaning up files

--- vision/tmp/drc_core_utils-cache-.cmake
+++
@@ -1,2 +0,0 @@
-
-set(CMAKE_INSTALL_PREFIX "/home/drc/object_detection/vision" CACHE PATH "Initial cache" FORCE)
--- vision/tmp/object_detection-cache-.cmake
+++
@@ -1,3 +0,0 @@
-
-set(CMAKE_INSTALL_PREFIX "/home/drc/object_detection/vision" CACHE PATH "Initial cache" FORCE)
-set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Initial cache" FORCE)
